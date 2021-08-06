1da768301247aadbd70f5e003179dac31bbe997c
hasandg <hasan.dg@hotmail.com>
2021-08-06 10:56:10 +0300

    Message

--- src/Header.js
+++ src/Header.js
@@ -4,12 +4,23 @@ import './Header.css';
 function Header(props) {
     return (
         <div className='header'>
-            <img  className="header__logo"  src="my_shopping.png" />
+            <img className="header__logo" src="my_shopping.png"/>
             <div className="header__search">
                 <div className="header__searchInput"></div>
                 {/* LOGO */}
             </div>
-            <div className="header__nav"></div>
+            <div className="header__nav">
+                <div className="header__option">
+                    <span className="header__optionLineOne">
+                        Hello Guest
+                    </span>
+                    <span className="header__optionLineTwo">
+                        Sign in
+                    </span>
+                </div>
+                <div className="header__option"></div>
+                <div className="header__option"></div>
+            </div>
         </div>
     );
 }
